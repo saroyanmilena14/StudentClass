a8d58fb6d15b95fa3b35dbd36d252a393da49b9a
Milena Saroyan <milenasaroyan14@gmail.com>
2021-02-09 19:57:58 +0400

    commit -m "update"

--- src/matrixAll/Arrays/MatrixAll/Arrays.java
+++ src/matrixAll/Arrays/MatrixAll/Arrays.java
@@ -1,5 +1,5 @@
 package matrixAll.Arrays.MatrixAll;
-// ex.2  printing rows from up to down
+// ex.2  printing columns from up to down
 public class Arrays {
     public static void main(String[] args) {
 
@@ -7,10 +7,11 @@ public class Arrays {
                 {8, 75, 21, 23}, // i=0
                 {17, 82, 61, 5}, // i=1
                 {71, 8, 28, 31}, // i=2
-                {12, 47, 32, 3} // i=3
+                {12, 47, 32, 3} ,// i
+                {34,5, 6, 7 }
         };
-        for (int j = 0; j < 4; j++) {
-            for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            for (int i = 0; i <matrix.length; i++) {
                 System.out.println(matrix[i][j] + " ");
             }
             System.out.println();
