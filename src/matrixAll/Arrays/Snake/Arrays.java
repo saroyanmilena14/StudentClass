f7f99b5424704eb220b19f3ef83ea307c05fd669
Milena Saroyan <milenasaroyan14@gmail.com>
2021-02-09 20:22:09 +0400

    star pattern commit

---
+++ src/matrixAll/Arrays/Snake/Arrays.java
@@ -0,0 +1,18 @@
+package matrixAll.Arrays.Snake;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int matrix[][] = new int[][]{
+                {8, 75, 21, 23}, // i=0
+                {17, 82, 61, 5}, // i=1
+                {71, 8, 28, 31}, // i=2
+                {12, 47, 32, 3} // i=3
+        };
+        int i;
+        int j;
+
+
+    }
+}
---
+++ src/starsTriangle/RightStarPattern.java
@@ -0,0 +1,20 @@
+package starsTriangle;
+
+public class RightStarPattern {
+
+
+    public static void main(String[] args) {
+
+        int i, j;
+        for(i=0; i<5; i++) {
+
+            for(j=0; j<i+1; j++) {
+
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
