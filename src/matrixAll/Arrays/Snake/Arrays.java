6046968c79457def19a18f2ec13c1bcf11a8e416
Milena Saroyan <milenasaroyan14@gmail.com>
2021-02-09 21:14:34 +0400

    commit -m "update snake"

--- src/matrixAll/Arrays/Snake/Arrays.java
+++ src/matrixAll/Arrays/Snake/Arrays.java
@@ -5,14 +5,35 @@ public class Arrays {
     public static void main(String[] args) {
 
         int matrix[][] = new int[][]{
-                {8, 75, 21, 23}, // i=0
-                {17, 82, 61, 5}, // i=1
-                {71, 8, 28, 31}, // i=2
-                {12, 47, 32, 3} // i=3
+                {8, 75, 21, 23},
+                {17, 82, 61, 5},
+                {71, 8, 28, 31},
+                {12, 47, 32, 3},
+
         };
         int i;
         int j;
 
 
+             for(i=0,j=0; j<matrix[0].length; j++) {
+                 System.out.println(matrix[i][j]);
+             }
+             for(j=matrix[0].length-1, i=1; i<matrix.length; i++){
+                 System.out.println(matrix[i][j]);
+             }
+
+             for(i=matrix.length-1, j=matrix[0].length-2; j>=0; j--) {
+                 System.out.println(matrix[i][j]);
+             }
+             for(j=0, i=matrix.length-2;i>=matrix.length-3; i--) {
+                 System.out.println(matrix[i][j]);
+             }
+              for(i=1, j=matrix[0].length-3; j<=matrix[0].length-2; j++) {
+                  System.out.println(matrix[i][j]);
+              }
+              for(i=2, j=matrix[0].length-2; j>=matrix[0].length-3; j--) {
+                  System.out.println(matrix[i][j]);
+              }
+         }
+
     }
-}
